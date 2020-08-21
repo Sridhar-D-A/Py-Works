ef35b62f6738e51dfad4aa4bc552925e2745dec8
sridhar.dhandapani <sridhardhandapani42@gmail.com>
2020-08-21 21:37:49 +0530

    Function to Display Monthly & Yearly calendar

---
+++ Simple Sys/calendarBasics.py
@@ -0,0 +1,13 @@
+import calendar
+
+def monthlyCalendar(year , month):
+     return calendar.month(year , month)
+
+
+def yearlyCalendar(year):
+    return calendar.calendar(year)
+
+
+# Year Sample - > 2020 , 2001 , 2050
+# Month Sample - > 1,2,3,4,5,6,7,8,9,10,11,12
+
